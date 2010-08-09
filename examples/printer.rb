160658bff8d99954932e9ba0a41411057a9f0b58
Caio Chassot <dev@caiochassot.com>
2010-08-09 17:52:08 -0300

    examples/printer: update to use new FileEvent block form; added comments on trap/join

--- examples/printer.rb
+++ examples/printer.rb
@@ -3,15 +3,9 @@
 $:.unshift File.join(File.dirname(__FILE__), '../lib')
 require 'file_event'
 
-class PrinterFileEvent < FSEvent::FileEvent
-  def on_file_change(path)
-    puts "File change detected: #{path}"
-  end
-end
-
-watcher = PrinterFileEvent.watch("/Users/kch/Desktop", :latency => 0.1)
+watcher = FSEvent::FileEvent.watch("/Users/kch/Desktop") { |path| puts "File change detected: #{path}" }
 
 puts "Watching #{watcher.directories.join(', ')}…"
 
-trap(:INT) { puts; exit }
-CFRunLoop.instance.thread.join
+trap(:INT) { puts; exit }      # ^C will exit gracefully
+CFRunLoop.instance.thread.join # Wait for CFRunLoop
